46dc70669a3075bd00b9b065ec1e9361652a70e6
gangjinzhang <729777679@qq.com>
2016-11-29 14:30:52 +0800

    Babel-Browserify-ES6

---
+++ README.md
@@ -0,0 +1,4 @@
+browserify-babel-demo
+---------------------
+
+使用Babel和Browserify创建你的ES6项目
---
+++ modules/index.js
@@ -0,0 +1,13 @@
+import { sum, square, variable, MyClass } from './import.js';
+// 25
+console.log(square(5));
+
+var cred = {
+    name: 'Ritesh Kumar',
+    enrollmentNo: 11115078
+}
+
+var x = new MyClass(cred);
+
+//Ritesh Kumar
+console.log(x.getName());
